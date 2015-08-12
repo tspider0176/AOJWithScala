6e2fb17fbf565641dd0cf88805f3a6160a1b37b0
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2015-08-13 00:27:02 +0900

    solved

--- ITP09-String/ITP1-9A.scala
+++ ITP09-String/ITP1-9A.scala
@@ -5,16 +5,8 @@ object Main{
     val line =
       for{
         in <-Source.stdin.getLines.toList
-      } yield in
+      } yield in.map(c => c.toLower)
 
-    def countNumOfStr(s:String, w:String):Int ={
-      if(s.contains(w) == false) 0
-      else if(s.contains(w) == true) 1 + countNumOfStr(s.drop(s.indexOfSlice(w) + w.length), w)
-      else 0
-    }
-
-    println(line.tail.mkString("\n"))
-
-    println(countNumOfStr(line.tail.mkString("\n"), line(0)))
+    println(line.tail.mkString(" ").split(" ").filter(_ == line(0)).length)
   }
 }
