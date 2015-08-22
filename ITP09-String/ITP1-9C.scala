5658b9b1792b4028268d421e640b5c52c7bbcc60
string <1a38d3a10f5e8.16h@gmail.com>
2015-08-22 17:06:49 +0900

    solved

--- ITP09-String/ITP1-9C.scala
+++ ITP09-String/ITP1-9C.scala
@@ -4,9 +4,23 @@ object Main{
   def main(args: Array[String]){
     val lines =
       for{
-      in <- Source.stdin.getLines.toString.toList
-    }yield in
+      in <- Source.stdin.getLines.toList
+    }yield in.toString
 
-    val point_a = "なんとかかんとか"
+    val point1 =
+      lines.tail.map(_.toString.split(" ").toList).map{
+        case x if(x(0).compare(x(1)) == 0) => 1
+        case x if(x(0).compare(x(1)) > 0 ) => 3
+        case _ => 0
+      }.sum
+
+    val point2 =
+      lines.tail.map(_.toString.split(" ").toList).map{
+        case x if(x(0).compare(x(1)) < 0 ) => 3
+        case x if(x(0).compare(x(1)) == 0) => 1
+        case _ => 0
+      }.sum
+
+    println(point1 + " " + point2)
   }
 }
