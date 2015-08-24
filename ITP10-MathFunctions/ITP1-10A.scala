606a90b613d88fb9ccf40a5936ee7991e1262e5c
string <1a38d3a10f5e8.16h@gmail.com>
2015-08-24 15:27:18 +0900

    solved

--- ITP10-MathFunctions/ITP1-10A.scala
+++ ITP10-MathFunctions/ITP1-10A.scala
@@ -3,11 +3,11 @@ import scala.math.{pow, sqrt}
 
 object Main{
   def main(args: Array[String]){
-    val in = for{
+    val lines = for{
       in <- Source.stdin.getLines.toList
     } yield in
 
-    val data = in.head.split(" ").toList.map(_.toDouble)
+    val data = lines.head.split(" ").toList.map(_.toDouble)
     println(sqrt(pow(data(2) - data(0), 2) + pow(data(3) - data(1), 2)))
   }
 }
--- ITP10-MathFunctions/ITP1-10B.scala
+++ ITP10-MathFunctions/ITP1-10B.scala
@@ -1,5 +1,15 @@
+import scala.io.Source
+import scala.math._
+
 object Main{
   def main(args: Array[String]){
-    
+    val lines = for{
+      in <- Source.stdin.getLines.toList
+    } yield in
+
+    val data =  lines.head.split(" ").toList.map(_.toInt)
+    println(data(0) * data(1) * sin(data(2) * Pi / 180) / 2)
+    println(data(0) + data(1) + sqrt(pow(data(0), 2) + pow(data(1), 2) - 2 * data(0) * data(1) * cos(data(2) * Pi /180)))
+    println(data(1) * sin(data(2) * Pi / 180))
   }
 }
