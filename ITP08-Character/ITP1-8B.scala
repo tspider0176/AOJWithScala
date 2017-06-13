cf98dd4cc1a2a92604a0a90e25b8a952d4b52ef2
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-13 23:27:41 +0900

    Modify 1-8B to using sum method

--- ITP08-Character/ITP1-8B.scala
+++ ITP08-Character/ITP1-8B.scala
@@ -3,7 +3,7 @@ import io.Source.stdin
 object Main{
   def main(args: Array[String]){
     for(line <- stdin.getLines().toList){
-      val sum = line.map{c => c.toString.toInt}.foldLeft(0)((acc, a) => acc + a)
+      val sum = line.map{c => c.toString.toInt}.sum
 
       if (sum != 0){
         println(sum)
