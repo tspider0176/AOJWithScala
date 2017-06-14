a30ca64a274134b2e229036dddc5fda329dcb206
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-14 18:27:18 +0900

    Modify input method in 1-2D

--- ITP02-BranchOnCondition/ITP1-2D.scala
+++ ITP02-BranchOnCondition/ITP1-2D.scala
@@ -2,24 +2,15 @@ import scala.io.StdIn
 
 object Main{
   def main(args: Array[String]){
-    var list = List[Int]()
+    val list = StdIn.readLine.split(" ").map(_.toInt)
 
-    for(in <- StdIn.readLine.split(" ").map(_.toInt)){
-      list = in :: list
-    }
-
-    list = list.reverse
     val w = list(0)
     val h = list(1)
     val x = list(2)
     val y = list(3)
     val r = list(4)
 
-    if((x+r) > w || (x-r) < 0 || (y+r) > h || (y-r) < 0){
-      println("No")
-    }
-    else{
-      println("Yes")
-    }
+    if((x+r) > w || (x-r) < 0 || (y+r) > h || (y-r) < 0) println("No")
+    else println("Yes")
   }
 }
