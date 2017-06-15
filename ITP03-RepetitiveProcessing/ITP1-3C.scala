950f02c2c70fb901f337fedda37f00478297cee2
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-15 19:46:09 +0900

    Modify 1-3C to using method

--- ITP03-RepetitiveProcessing/ITP1-3C.scala
+++ ITP03-RepetitiveProcessing/ITP1-3C.scala
@@ -2,22 +2,11 @@ import scala.io.Source
 
 object Main{
   def main(args: Array[String]){
-    var list = List[String]()
-    for(in <- Source.stdin.getLines.toList){
-      list = in :: list
-    }
+    val line =
+      for{
+        in <- Source.stdin.getLines.toList
+      } yield in
 
-    for(i <- 0 until list.length){
-      val arr = list(list.length - i - 1).split(Array(' ')).map(_.toInt)
-
-      if(!(arr(0) == 0 && arr(1) == 0)){
-        if(arr(0) > arr(1)){
-          println(arr(1) + " " + arr(0))
-        }
-        else{
-          println(arr(0) + " " + arr(1))
-        }
-      }
-    }
+    println(line.map{str => str.split(" ").map(_.toInt).sorted.mkString(" ")}.init.mkString("\n"))
   }
 }
