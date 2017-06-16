2523a688b792267e6ada237ff76665ba078cf7d4
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-16 17:32:16 +0900

    Modify 1-4A

--- ITP04-Computation/ITP1-4A.scala
+++ ITP04-Computation/ITP1-4A.scala
@@ -1,13 +1,9 @@
 import scala.io.StdIn
- 
+
 object Main{
   def main(args: Array[String]){
-    var list = List[Int]()
- 
-    for(in <- StdIn.readLine.split(" ").map(_.toInt)){
-      list = in :: list
-    }
- 
-    printf("%d %d %10.8f\n", list(1) / list(0), list(1) % list(0),list(1).toDouble / list(0).toDouble)
+    val list = StdIn.readLine.split(" ").toList.map(_.toInt)
+
+    printf("%d %d %10.8f\n", list(0) / list(1), list(0) % list(1), list(0).toDouble / list(1).toDouble)
   }
 }
