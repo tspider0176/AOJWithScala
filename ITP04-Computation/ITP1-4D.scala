485b7d4d83228cc20f1e25ea3a2d13de550973ec
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-16 18:17:07 +0900

    Modify 1-4D

--- ITP04-Computation/ITP1-4D.scala
+++ ITP04-Computation/ITP1-4D.scala
@@ -1,13 +1,9 @@
+import scala.io.StdIn
+
 object Main{
   def main(args: Array[String]){
-    val sc = new java.util.Scanner(System.in)
-    val num = sc.nextInt
-    var list = List[Long]()
-
-    for(i <- 1 to num){
-      val in = sc.nextInt
-      list = in :: list
-    }
+    StdIn.readLine()
+    val list = StdIn.readLine.split(" ").map(_.toLong).toList
 
     println(list.min + " " + list.max + " " + list.sum)
   }
