b3673e6b27efc96c2335565e8cb00317f6ce6665
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-17 23:18:55 +0900

    Modify 1-6C

--- ITP06-Array/ITP1-6C.scala
+++ ITP06-Array/ITP1-6C.scala
@@ -2,12 +2,12 @@ import scala.io.StdIn
 
 object Main{
   def main(args: Array[String]){
-    val num = StdIn.readLine()
-    var arr = Array.ofDim[Int](4,3,10)
+    val num = StdIn.readLine().toInt
+    val arr = Array.ofDim[Int](4,3,10)
 
-    for(i <- 1 to num.toInt){
-      val in = StdIn.readLine().split(" ")
-      arr(in(0).toInt-1)(in(1).toInt-1)(in(2).toInt-1) += in(3).toInt
+    (1 to num).foreach{i =>
+      val in = StdIn.readLine().split(" ").map(_.toInt).toList
+      arr(in(0) - 1)(in(1) - 1)(in(2) - 1) += in(3)
     }
 
     for(i <- 0 until 4){
