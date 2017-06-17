b0522f45dae609984b595c148ec4b651b1a2bc48
tspider0176 <1a38d3a10f5e8.16h@gmail.com>
2017-06-17 23:03:33 +0900

    Modify 1-5C

--- ITP05-StructuredProgramI/ITP1-5C.scala
+++ ITP05-StructuredProgramI/ITP1-5C.scala
@@ -2,35 +2,23 @@ import scala.io.Source
 
 object Main{
   def main(args: Array[String]){
-    var list = List[String]()
-    for(in <- Source.stdin.getLines.toList){
-      list = in :: list
-    }
+    val list =
+      for{
+        in <- Source.stdin.getLines.toList
+      } yield in
 
-    for(i <- 1 to list.length){
-      val arr = list(list.length - i).split(Array(' '))
+    list.map(_.split(" ").map(_.toInt).toList).init.foreach{ list =>
+      val height = list.head
+      val width = list.last
+      val bw = "#."
 
-      if(!(arr(0).toInt == 0 && arr(1).toInt == 0)){
-        for(i <- 0 until arr(0).toInt){
-          for(j <- 0 until arr(1).toInt){
-            i match{
-              case x if(x % 2 == 0) => j match{
-                  case y if(y % 2 == 0) => print("#")
-                  case y if(y % 2 == 1) => print(".")
-                  case _ => None
-                }
-              case x if(x % 2 == 1) => j match{
-                  case y if(y % 2 == 0) => print(".")
-                  case y if(y % 2 == 1) => print("#")
-                  case _ => None
-                }
-              case _ => None
-            }
-          }
-          println
+      (1 to height).foreach{ row =>
+        row match{
+          case x if x % 2 == 0 => println((bw.reverse * width).take(width))
+          case x if x % 2 == 1 => println((bw * width).take(width))
         }
-        println
       }
+      println
     }
   }
 }
